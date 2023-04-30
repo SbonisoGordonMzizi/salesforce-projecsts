086632dfbe0ba82815a070f42ea674d9f31b1e79
SbonisoGordonMzizi <gordonmzizi@gmail.com>
2023-04-30 23:21:34 +0200

    lightning web component

---
+++ NavigationApp/force-app/main/default/lwc/navigateToHome/navigateToHome.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import {NavigationMixin} from 'lightning/navigation';
+export default class NavigateToHome extends LightningElement {
+
+    navigateToHome(){
+
+    }
+}
